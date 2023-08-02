a3b47f3e4702f597a6652a4042e96fc9bb12762d
b1ek <me@blek.codes>
2023-08-02 20:21:49 +1000

    add benchmarks to example

--- examples/simple_pow.rs
+++ examples/simple_pow.rs
@@ -1,7 +1,18 @@
 
-use powlib::{self, gen::{POWRange, POWChallenge}};
+use std::time::Instant;
+
+use powlib::{self, gen::{POWRange, POWChallenge}, hash::hash_num, num::Num};
 
 fn main() {
-    let challenge = POWChallenge::make(POWRange::new(0, 20480));
-    println!("{}", challenge.solve_singlethread());
+    {
+        let num = Num::new(473823);
+        let time = Instant::now();
+        hash_num(num);
+        println!("Simple hash of {} took {} millis", u128::from(num), time.elapsed().as_millis());
+    }
+    {
+        let time = Instant::now();
+        let challenge = POWChallenge::make(POWRange::new(0, 20480));
+        println!("Solvling {} took {} seconds", challenge.solve_singlethread(), time.elapsed().as_secs_f32());
+    }
 }
